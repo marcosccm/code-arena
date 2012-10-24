e580890341cef6dbab1b3dbdf714ecbf690c11f6
marcosccm <marcosccm@gmail.com>
2012-10-23 22:52:01 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codearena::Application.config.secret_token = '6ff56145a3d3b7b40c208a1e2d82de87340c626a9e7e83cb53d2b31133b6394984fdf82c06b213f4f35424ba3a1cec1466ec7bdb7950efbe55cbf4204f53164a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Codearena::Application.config.session_store :cookie_store, key: '_codearena_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Codearena::Application.config.session_store :active_record_store
