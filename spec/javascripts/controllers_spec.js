565d82e3500650e114f580006dc77bc9b34b4a6d
marcosccm <marcosccm@gmail.com>
2012-10-30 09:11:44 -0200

    basic test for submiting challenges entries

--- spec/javascripts/controllers_spec.js
+++ spec/javascripts/controllers_spec.js
@@ -3,14 +3,28 @@ describe('EntryListCrtl', function(){
 
   beforeEach(function() {
     scope = {};
-    entryService = { query: function(){} };
+    entryService = { 
+      query: function(){}, 
+      save: function(e,cb) { cb(); }  
+    };
     entries = ['entry1', 'entry2'];
   });
 
   it('instatiate entries from server response', function(){
     spyOn(entryService, 'query').andReturn(entries)
-
     crtl  = new EntryListCrtl(scope, entryService);
     expect(scope.entries).toEqual(entries);
+  });
+
+  describe('submiting challenges entries', function(){
+    beforeEach(function(){
+      crtl  = new EntryListCrtl(scope, entryService);
+    });
+
+    it('saves the entry', function(){
+      spyOn(entryService, 'save')
+      scope.submit();
+      expect(entryService.save).toHaveBeenCalled();
+    });
   });
 });
