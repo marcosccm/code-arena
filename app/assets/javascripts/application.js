f78b717155dcb75dd22eeeb1581f54f696eabdb6
marcosccm <marcosccm@gmail.com>
2012-12-07 00:56:43 -0200

    putting templates on their own files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,19 +3,4 @@
 //= require controllers
 //= require services
 //= require angular-resource
-
-angular.module('codearena',['entryService']).config(function($routeProvider){
-  $routeProvider
-  .when('/', { 
-    controller: CurrentChallengeCrtl, 
-    templateUrl: 'current_challenge.html' 
-  })
-  .when('/edit', { 
-    controller: EditChallengeCrtl, 
-    templateUrl: 'edit_challenge.html' 
-  })
-  .when('/new', { 
-    controller: AddChallengeEntryCrtl, 
-    templateUrl: 'add_challenge_entry.html' 
-  });
-});
+//= require codearena
