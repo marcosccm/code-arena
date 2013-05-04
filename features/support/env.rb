25a0ccc63be09d271ac74c42392413bf7020f42c
Marcos Matos <marcosccm@gmail.com>
2013-05-04 15:44:03 -0300

    use poltergeist as capybara js driver

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,18 @@
 require_relative 'fake_data_store'
 require_relative 'fake_github_auth'
 require_relative 'application_runner'
+require 'capybara/poltergeist'
 require 'cucumber/rails'
 
 Capybara.default_selector  = :css
-Capybara.javascript_driver = :selenium
+Capybara.javascript_driver = :poltergeist
+
+Capybara.configure do |config|
+  config.match = :one
+  config.exact_options = true
+  config.ignore_hidden_elements = true
+  config.visible_text_only = true
+end
 
 ActionController::Base.allow_rescue = false
 Cucumber::Rails::Database.javascript_strategy = :truncation
