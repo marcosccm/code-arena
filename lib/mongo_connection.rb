a23bd3c1e408f3325239e42cfdb2a9e7fa0640cb
Marcos Matos <marcosccm@gmail.com>
2013-08-09 15:39:39 -0300

    bla

--- lib/mongo_connection.rb
+++ lib/mongo_connection.rb
@@ -2,7 +2,7 @@ class MongoConnection
   include Mongo
 
   def self.setup(configuration)
-    client = MongoClient.new(configuration.fetch(:host), configuration.fetch(:port))
+    client = Mongo::Client.connect("mongodb://test:whocares@ds037698.mongolab.com:37698/heroku_app8687179")
     @db    = client[configuration.fetch(:database)]
     if configuration[:user]
       @db.authenticate(configuration[:user], configuration[:password])
